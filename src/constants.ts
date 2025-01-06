1cb736c9c4f1712b938151b03ca8571a2b815849
pcparts001 <pcparts001@gmail.com>
2025-01-06 16:28:28 +0900

    a

--- src/constants.ts
+++ src/constants.ts
@@ -21,7 +21,7 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3'
+export const FACTORY_ADDRESS = '0xEE4bC42157cf65291Ba2FE839AE127e3Cc76f741'
 
 export const INIT_CODE_HASH = '0xcdf2deca40a0bd56de8e3ce5c7df6727e5b1bf2ac96f283fa9c4b3e6b42ea9d2'
 
